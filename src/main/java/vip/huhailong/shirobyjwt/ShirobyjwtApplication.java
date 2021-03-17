609fe3c1d0034704a9988bbac5bd1aecf765e06c
胡海龙 <hailong.hu@bj-tct.com>
2021-03-17 10:28:11 +0800

    初始化项目

---
+++ src/main/java/vip/huhailong/shirobyjwt/ShirobyjwtApplication.java
@@ -0,0 +1,13 @@
+package vip.huhailong.shirobyjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirobyjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShirobyjwtApplication.class, args);
+	}
+
+}
